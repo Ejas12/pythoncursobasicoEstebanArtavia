3409efd4ec908a83821eeb21a3458dbea4a442e2
Esteban Artavia <Esteban_Artavia@mckinsey.com>
2019-01-25 14:58:35 -0600

    adding tarea 1

---
+++ tarea1.py
@@ -0,0 +1,8 @@
+import math
+pi = math.pi
+asterisk = '*'
+#declare args for print
+printargs = asterisk, pi, asterisk*2, pi, asterisk*3, pi, asterisk*4, pi,asterisk*5, pi, asterisk*6, pi
+
+#print formatted data
+print("%s %7f %s %6f %s %5f %s %4f %s %3f %s %2f" %(printargs))
